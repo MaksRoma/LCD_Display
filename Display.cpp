4e7209d341bbd97dd8c208885c150cec1c66228e
MaksRoma <maksymiukroma@gmail.com>
2023-01-07 21:08:58 +0200

    major update (it's finally print character to LCD!!!!)

--- Display.cpp
+++ Display.cpp
@@ -1,186 +1,38 @@
 //--------------------------------------------------------------------------------------------------------------
 #include "Display.h"
 //--------------------------------------------------------------------------------------------------------------
-void Clear_Display()
+void Print_DBus(uint8_t data)
 {
-	digitalWrite(DB0,1);
-
-	digitalWrite(DB1,0);
-	digitalWrite(DB2,0);
-	digitalWrite(DB3,0);
-	digitalWrite(DB4,0);
-	digitalWrite(DB5,0);
-	digitalWrite(DB6,0);
-	digitalWrite(DB7,0);
-
-	digitalWrite(RS,0);
-  digitalWrite(RW,0);
-	digitalWrite(E,0);
+  digitalWrite(DB7,data & 0x1);
+  digitalWrite(DB6,data & 0x2);
+  digitalWrite(DB5,data & 0x4);
+  digitalWrite(DB4,data & 0x8);
 }
 //--------------------------------------------------------------------------------------------------------------
-void Return_Home()
+void LCD_Setup()
 {
-	digitalWrite(DB0,1);
-	digitalWrite(DB1,1);
-
-	digitalWrite(DB2,0);
-	digitalWrite(DB3,0);
-	digitalWrite(DB4,0);
-	digitalWrite(DB5,0);
-	digitalWrite(DB6,0);
-	digitalWrite(DB7,0);
-
-	digitalWrite(RS,0);
-  digitalWrite(RW,0);
-	digitalWrite(E,0);
-}
-//--------------------------------------------------------------------------------------------------------------
-void Set_Entry_Mode(bool db0,bool db1)
-{
-	digitalWrite(DB0,db0);
-	digitalWrite(DB1,db1);
-	digitalWrite(DB2,1);
-
-	digitalWrite(DB3,0);
-	digitalWrite(DB4,0);
-	digitalWrite(DB5,0);
-	digitalWrite(DB6,0);
-	digitalWrite(DB7,0);
-
-	digitalWrite(RS,0);
-  digitalWrite(RW,0);
-	digitalWrite(E,0);
-}
-//--------------------------------------------------------------------------------------------------------------
-void Set_Display(bool db0, bool db1, bool db2)
-{
-	digitalWrite(DB0,db0);
-	digitalWrite(DB1,db1);
-	digitalWrite(DB2,db2);
-	digitalWrite(DB3,1);
-  
-	digitalWrite(DB4,0);
-	digitalWrite(DB5,0);
-	digitalWrite(DB6,0);
-	digitalWrite(DB7,0);
-
-	digitalWrite(RS,0);
-  digitalWrite(RW,0);
-	digitalWrite(E,0);
-}
-//--------------------------------------------------------------------------------------------------------------
-void Shift_Display_OR_Cursor(bool db0, bool db1, bool db2, bool db3)
-{
-  digitalWrite(DB0,db0);
-	digitalWrite(DB1,db1);
-	digitalWrite(DB2,db2);
-	digitalWrite(DB3,db3);
-	digitalWrite(DB4,1);
-
-	digitalWrite(DB5,0);
-	digitalWrite(DB6,0);
-	digitalWrite(DB7,0);
-
-	digitalWrite(RS,0);
-  digitalWrite(RW,0);
-	digitalWrite(E,0);
-}
-//--------------------------------------------------------------------------------------------------------------
-void Set_Function(bool db0, bool db1, bool db2, bool db3, bool db4)
-{
-  digitalWrite(DB0,db0);
-	digitalWrite(DB1,db1);
-	digitalWrite(DB2,db2);
-	digitalWrite(DB3,db3);
-	digitalWrite(DB4,db4);
-	digitalWrite(DB5,1);
-
-	digitalWrite(DB6,0);
-	digitalWrite(DB7,0);
-
-	digitalWrite(RS,0);
-  digitalWrite(RW,0);
-	digitalWrite(E,0);
-}
-//--------------------------------------------------------------------------------------------------------------
-void Set_CGRAM_Address(bool db0, bool db1, bool db2, bool db3, bool db4, bool db5)
-{
-  digitalWrite(DB0,db0);
-	digitalWrite(DB1,db1);
-	digitalWrite(DB2,db2);
-	digitalWrite(DB3,db3);
-	digitalWrite(DB4,db4);
-	digitalWrite(DB5,db5);
-	digitalWrite(DB6,1);
-
-	digitalWrite(DB7,0);
-
-	digitalWrite(RS,0);
-  digitalWrite(RW,0);
-	digitalWrite(E,0);
-}
-//--------------------------------------------------------------------------------------------------------------
-void Set_DDRAM_Address(bool db0, bool db1, bool db2, bool db3, bool db4, bool db5, bool db6)
-{
-  digitalWrite(DB0,db0);
-	digitalWrite(DB1,db1);
-	digitalWrite(DB2,db2);
-	digitalWrite(DB3,db3);
-	digitalWrite(DB4,db4);
-	digitalWrite(DB5,db5);
-	digitalWrite(DB6,db6);
-	digitalWrite(DB7,1);
-
-	digitalWrite(RS,0);
-  digitalWrite(RW,0);
-	digitalWrite(E,0);
-}
-//--------------------------------------------------------------------------------------------------------------
-void Read_BF_and_Address(bool db0, bool db1, bool db2, bool db3, bool db4, bool db5, bool db6, bool db7)
-{
-  digitalWrite(DB0,db0);
-	digitalWrite(DB1,db1);
-	digitalWrite(DB2,db2);
-	digitalWrite(DB3,db3);
-	digitalWrite(DB4,db4);
-	digitalWrite(DB5,db5);
-	digitalWrite(DB6,db6);
-	digitalWrite(DB7,db7);
-
-	digitalWrite(RS,0);
-  digitalWrite(RW,1);
-	digitalWrite(E,0);
-}
-//--------------------------------------------------------------------------------------------------------------
-void Write_To_RAM(bool db0, bool db1, bool db2, bool db3, bool db4, bool db5, bool db6, bool db7)
-{
-  digitalWrite(DB0,db0);
-	digitalWrite(DB1,db1);
-	digitalWrite(DB2,db2);
-	digitalWrite(DB3,db3);
-	digitalWrite(DB4,db4);
-	digitalWrite(DB5,db5);
-	digitalWrite(DB6,db6);
-	digitalWrite(DB7,db7);
-
-	digitalWrite(RS,1);
-  digitalWrite(RW,0);
-	digitalWrite(E,0);
-}
-//--------------------------------------------------------------------------------------------------------------
-void Read_From_RAM(bool db0, bool db1, bool db2, bool db3, bool db4, bool db5, bool db6, bool db7)
-{
-  digitalWrite(DB0,db0);
-	digitalWrite(DB1,db1);
-	digitalWrite(DB2,db2);
-	digitalWrite(DB3,db3);
-	digitalWrite(DB4,db4);
-	digitalWrite(DB5,db5);
-	digitalWrite(DB6,db6);
-	digitalWrite(DB7,db7);
-
-	digitalWrite(RS,1);
-  digitalWrite(RW,1);
-	digitalWrite(E,0);
+  delay(50);
+  //set 8-bit mode
+  Print_DBus(0xC);
+  delayMicroseconds(50);
+  //set 4-bit mode,N = 1, 5x8 dot
+  Print_DBus(0x4);
+  Print_DBus(0x1);
+  delayMicroseconds(50);
+  //set 4-bit mode,N = 1, 5x8 dot
+  Print_DBus(0x4);
+  Print_DBus(0x1);
+  delayMicroseconds(50);
+  //turn on display
+  Print_DBus(0x0);
+  Print_DBus(0xF);
+  delayMicroseconds(50);
+  //clear display
+  Print_DBus(0x0);
+  Print_DBus(0x1);
+  delay(50);
+  //Entry mode set
+  Print_DBus(0x0);
+  Print_DBus(0x7);
 }
 //--------------------------------------------------------------------------------------------------------------
--- LCD_Display.ino
+++ LCD_Display.ino
@@ -1,25 +1,45 @@
 #include "Display.h"
 
+
 void setup()
 {
   pinMode(RS,OUTPUT);
-  pinMode(RW,OUTPUT);
   pinMode(E,OUTPUT);
 
-  pinMode(DB0,OUTPUT);
-  pinMode(DB1,OUTPUT);
-  pinMode(DB2,OUTPUT);
-  pinMode(DB3,OUTPUT);
+  // pinMode(DB0,OUTPUT);
+  // pinMode(DB1,OUTPUT);
+  // pinMode(DB2,OUTPUT);
+  // pinMode(DB3,OUTPUT);
   pinMode(DB4,OUTPUT);
   pinMode(DB5,OUTPUT);
   pinMode(DB6,OUTPUT);
   pinMode(DB7,OUTPUT);
-  
+
+  LCD_Setup();
 }
 
 void loop()
 {
-  // Clear_Display();
-  // Set_Entry_Mode(1,1);
-  // Set_Display(1,1,0);
+  //set cursor
+  //. . .
+
+
+  digitalWrite(RS, HIGH);
+  //clear display from previous using
+  Print_DBus(0x0);
+  Print_DBus(0x1);
+
+
+  //print upper-case 4-bits
+  Print_DBus(0x2);
+  //latching
+  digitalWrite(E, HIGH);
+  digitalWrite(E, LOW);
+  //print lower-case 4-bits
+  Print_DBus(0x8);
+  //latching
+  digitalWrite(E, HIGH);
+  digitalWrite(E, LOW);
+  
+  delay(250);
 }
