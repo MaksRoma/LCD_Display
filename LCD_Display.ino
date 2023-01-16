fed8644cc9ae5cc8a6fc333a7b9a62c2a68d4d4d
MaksRoma <maksymiukroma@gmail.com>
2023-01-16 21:43:29 +0200

    add function for print strin or decimal/hexadecimal to LCD

--- LCD_Display.ino
+++ LCD_Display.ino
@@ -1,12 +1,11 @@
 #include "Display.h"
 #include "CGRAM.h"
-
-
+#include <LiquidCrystal.h>
+//--------------------------------------------------------------------------------------------------------------
 const char welcome[] = "Hello, World!";
 const char str[] = "MaksRoma here";
 uint8_t i;
-
-
+//--------------------------------------------------------------------------------------------------------------
 void setup()
 {
 
@@ -20,41 +19,24 @@ void setup()
 
   LCD_Setup();
 
-  Print_CMD(0x40);
-  Set_CGRAM(_Pattern_1);
-  Set_CGRAM(_Pattern_2);  
-  Set_CGRAM(_Pattern_3); 
-  Set_CGRAM(_Pattern_4);  
-  Set_CGRAM(_Pattern_5);  
-  Set_CGRAM(_Pattern_6);  
-  Set_CGRAM(_Pattern_2);
+  //uncomment, if you want use custom characters
+  // Print_CMD(0x40);
+  // Set_CGRAM(_Pattern_1);
+  // Set_CGRAM(_Pattern_2);
+  // Set_CGRAM(_Pattern_3);
+  // Set_CGRAM(_Pattern_4);
+  // Set_CGRAM(_Pattern_5);
+  // Set_CGRAM(_Pattern_6);
+  // Set_CGRAM(_Pattern_7);
+  // Set_CGRAM(_Pattern_8);
   
   Print_CMD(0x80);
-  // first 4 characters from CGRAM
-  for(i = 0; i < 4; i++)
-    Print_Data(i);
-  Print_Data(0x79);
-  //5-th character from CGRAM
-  Print_Data(4);
-  Print_Data(0x20);
-  //6-th character from CGRAM
-  Print_Data(5);
-  //7-th character from CGRAM
-  Print_Data(1);
-  Print_Data(0x79);
-
 
   Print_CMD(0xC0);
-  Print_To_LCD("Hello, World!");
-  Print_To_LCD(26);
-
-  // Print_CMD(0xC0);
-  // delay(50);
-  // for(i = 0; i < sizeof(welcome) - 1;i++)
-  //   Print_Data(str[i]);
 }
-
+//--------------------------------------------------------------------------------------------------------------
 void loop()
 {
 
 }
+//--------------------------------------------------------------------------------------------------------------
